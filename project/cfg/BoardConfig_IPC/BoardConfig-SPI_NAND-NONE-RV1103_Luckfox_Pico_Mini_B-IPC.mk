f840ab9fe699569c21d2b02fd417bdb467407aa1
luckfox-eng33 <144299491+luckfox-eng33@users.noreply.github.com>
2023-10-28 16:06:02 +0800

    feat: add luckfox pico mini a/b support (#14)
    * fix:wifi_app mistake
    
    * feat: add luckfox pico mini a/b support
    
    Signed-off-by: eng33 <eng33@luckfox.com>
    
    * Modified: Change the board name luckfox_pico_max to luckfox_pico__pro_max
    
    Signed-off-by: eng33 <eng33@luckfox.com>
    
    ---------
    
    Signed-off-by: eng33 <eng33@luckfox.com>

--- project/cfg/BoardConfig_IPC/BoardConfig-SPI_NAND-NONE-RV1103_Luckfox_Pico_Mini_B-IPC.mk
+++ project/cfg/BoardConfig_IPC/BoardConfig-SPI_NAND-NONE-RV1103_Luckfox_Pico_Mini_B-IPC.mk
@@ -22,7 +22,7 @@ export RK_UBOOT_DEFCONFIG_FRAGMENT=rk-sfc.config
 export RK_KERNEL_DEFCONFIG=luckfox_rv1106_linux_defconfig
 
 # Kernel dts
-export RK_KERNEL_DTS=rv1106g-luckfox-pico-max.dts
+export RK_KERNEL_DTS=rv1103g-luckfox-pico-mini-b.dts
 
 #misc image
 export RK_MISC=wipe_all-misc.img
@@ -39,8 +39,8 @@ export RK_CAMERA_SENSOR_CAC_BIN="CAC_sc4336_OT01_40IRC_F16"
 #export RK_CAMERA_SENSOR_CAC_BIN="CAC_sc4336_OT01_40IRC_F16 CAC_sc530ai_CMK-OT2115-PC1_30IRC-F16"
 
 # Config CMA size in environment
-# export RK_BOOTARGS_CMA_SIZE="24M"
-export RK_BOOTARGS_CMA_SIZE="66M"
+export RK_BOOTARGS_CMA_SIZE="24M"
+#export RK_BOOTARGS_CMA_SIZE="66M"
 
 # config partition in environment
 # RK_PARTITION_CMD_IN_ENV format:
@@ -48,7 +48,7 @@ export RK_BOOTARGS_CMA_SIZE="66M"
 #       <partdef> := <size>[@<offset>](part-name)
 # Note:
 #   If the first partition offset is not 0x0, it must be added. Otherwise, it needn't adding.
-export RK_PARTITION_CMD_IN_ENV="256K(env),256K@256K(idblock),512K(uboot),4M(boot),32M(rootfs),48M(oem),160M(userdata)"
+export RK_PARTITION_CMD_IN_ENV="256K(env),256K@256K(idblock),512K(uboot),4M(boot),32M(rootfs),48M(oem),32M(userdata)"
 #export RK_PARTITION_CMD_IN_ENV="256K(env),256K@256K(idblock),256K(uboot),8M(boot),32M(rootfs),48M(oem),32M(userdata),-(media)"
 
 # config partition's filesystem type (squashfs is readonly)
@@ -70,8 +70,8 @@ export RK_PARTITION_FS_TYPE_CFG=rootfs@IGNORE@ubifs,oem@/oem@ubifs,userdata@/use
 # export RK_UBIFS_COMP=lzo
 
 # app config
-# export RK_APP_TYPE=RKIPC_RV1103
-export RK_APP_TYPE=RKIPC_RV1106
+export RK_APP_TYPE=RKIPC_RV1103
+# export RK_APP_TYPE=RKIPC_RV1106
 
 # build ipc web backend
 # export RK_APP_IPCWEB_BACKEND=y
