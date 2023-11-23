3663d61939ac033ebfa0efefa437cb9a01d35a97
Alistair Jordan <alistairjordan>
2023-11-23 20:30:55 +0000

    CICD test

---
+++ cicd/prepare_runner.sh
@@ -0,0 +1,25 @@
+set -e
+
+# Install binfmt-support and zip
+sudo apt install -y binfmt-support qemu-user-static zip
+
+# Install docker
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Do submodule checkout
+git submodule init
+git submodule update --depth=1
