213778278badc22453d7fa6cdffeb2cf6d102c3e
Sergei Rebrov <cerjhub@gmail.com>
2024-09-11 23:59:14 +0300

    printf fix

--- media/luckfox/src/luckfox_spi.c
+++ media/luckfox/src/luckfox_spi.c
@@ -49,13 +49,13 @@ int luckfox_spi_init(char *dev, uint32_t mode, uint8_t bits, uint32_t speed)
    ret = ioctl(fd, SPI_IOC_WR_MODE, &mode);
    if (ret == -1)
    {
-      printf("SPI_IOC_RD_MODE error......\n ");
+      printf("SPI_IOC_WR_MODE error......\n ");
       goto fd_close;
    }
    ret = ioctl(fd, SPI_IOC_RD_MODE, &mode);
    if (ret == -1)
    {
-      printf("SPI_IOC_WR_MODE error......\n ");
+      printf("SPI_IOC_RD_MODE error......\n ");
       goto fd_close;
    }
 
@@ -63,13 +63,13 @@ int luckfox_spi_init(char *dev, uint32_t mode, uint8_t bits, uint32_t speed)
    ret = ioctl(fd, SPI_IOC_WR_BITS_PER_WORD, &bits);
    if (ret == -1)
    {
-      printf("SPI_IOC_RD_BITS_PER_WORD error......\n ");
+      printf("SPI_IOC_WR_BITS_PER_WORD error......\n ");
       goto fd_close;
    }
    ret = ioctl(fd, SPI_IOC_RD_BITS_PER_WORD, &bits);
    if (ret == -1)
    {
-      printf("SPI_IOC_WR_BITS_PER_WORD error......\n ");
+      printf("SPI_IOC_RD_BITS_PER_WORD error......\n ");
       goto fd_close;
    }
 
